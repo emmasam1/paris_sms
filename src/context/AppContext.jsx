05fde1fe1fb6b4060c4ad81ed74cd701974058b7
emmanuel sam <fndm4@gmail.com>
2025-12-23 12:55:51 +0100

    changed baseUrl

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -6,7 +6,8 @@ import { useNavigate } from "react-router";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const API_BASE_URL = "https://scholas-v2.onrender.com";
+  // const API_BASE_URL = "https://scholas-v2.onrender.com";
+  const API_BASE_URL = "https://scholas.onrender.com";
   const SECRET_KEY = import.meta.env.VITE_SECRET_KEY || "fallback-secret-key";
 
   const navigate = useNavigate(); // ✅ FIXED
